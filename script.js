350c13049c53505c288ff3f590f8cf23f935425f
Liv <livbendix@live.dk>
2024-02-02 11:22:45 +0100

    hjkj

--- script.js
+++ script.js
@@ -3,10 +3,18 @@
 // console.log(`my name is ${firstName}`);
 //}
 
-momsBeregner(200);
+//momsBeregner(200);
+
+//function momsBeregner(beloeb, moms = 25) {
+//const total = beloeb * (1 + moms / 100);
+
+// console.log(total);
+//}
 
 function momsBeregner(beloeb, moms = 25) {
-  const total = beloeb * (1 + moms / 100);
-
-  console.log(total);
+  return beloeb * (1 + moms / 100);
 }
+
+let beregnetMoms = momsBeregner(100);
+console.log("beregnetMoms", beregnetMoms);
+aare;
